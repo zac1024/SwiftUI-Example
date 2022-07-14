05d7f1d375cfbaa21bd7293b28beec0ed603b154
peierlong <peierlong@youlu.com>
2022-07-14 15:37:23 +0800

    update ProgressView code

--- example_01/ProgressViewExample/ProgressViewExample/ContentView.swift
+++ example_01/ProgressViewExample/ProgressViewExample/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            ProgressView()
-        }
+        
+        ProgressView("loading ...")
+        
     }
 }
 
