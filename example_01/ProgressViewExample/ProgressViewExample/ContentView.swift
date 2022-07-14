37b1a0766c7dc05f8e2759ba4f78b3bb4079e516
peierlong <peierlong@youlu.com>
2022-07-14 15:12:59 +0800

    add example 1

---
+++ example_01/ProgressViewExample/ProgressViewExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ProgressViewExample
+//
+//  Created by erick on 2022/7/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ProgressView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ example_01/ProgressViewExample/ProgressViewExample/ProgressViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewExampleApp.swift
+//  ProgressViewExample
+//
+//  Created by erick on 2022/7/14.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
